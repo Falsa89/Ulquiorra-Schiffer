5781ea0db8190821870a8b88cb9dd1fb69433b0f
Falsa89 <47703851+Falsa89@users.noreply.github.com>
2020-07-22 04:38:54 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -18,35 +18,71 @@ $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
 $response = '';
-if(strpos($text, "/start") === 0 || $text=="ciao")
+if(strpos($text, "/start") === 0 || $text=="espada")
 {
-	$response = "..........!";
+	$response = "......!";
 }
-elseif (strpos($text, "nome") !== false)
+elseif (strpos($text, "vita") !== false)
 {
-	$response = "L'avevo dimenticato da molto tempo ... il dolore di non avere un nome. Quegli altri ragazzi avevano nomi che altri li chiamavano, ma io no.!";
+	$response = "Le cose che venivano riflesse nei miei occhi non avevano significato. Le cose che non potevano essere riflesse nei miei occhi non esistevano.";
 }
-elseif (strpos($text, "forte") !== false)
+elseif (strpos($text, "significato") !== false)
 {
-	$response = "Voglio diventare più forte. Ho finalmente trovato un degno avversario. POSSO diventare più forte. Voglio combattere. VOGLIO diventare più forte. Di certo è passato un po 'di tempo dall'ultima volta che ho avuto questo desiderio. Voglio diventare più forte .";
+	$response = "Il nostro mondo non ha alcun significato, neppure noi che ci viviamo abbiamo alcun significato, noi.... privi di significato, pensiamo al mondo nonostante anche l’essere consci che non abbia significato farlo, sia privo di significato";
 }
-elseif (strpos($text, "spammando") !== false)
+elseif (strpos($text, "desider") !== false)
 {
-	$response = "Blah blah blah! Sei rumoroso! Vieni e combatti. Anzi, è meglio se venite tutti quanti insieme. Se mi circondate ed attaccate contemporaneamente, forse uno di voi mi ferirà davvero..";
+	$response = "Possiedo un cuore dunque invidio, possiedo un cuore dunque divoro, possiedo un cuore dunque depredo, possiedo un cuore dunque sono pigro, possiedo un cuore dunque sono superbo, possiedo un cuore dunque mi adiro, possiedo un cuore dunque desidero tutto!";
 }
-elseif (strpos($text, "pazzo") !== false)
+elseif (strpos($text, "donna") !== false)
 {
-	$response = "Sanità mentale? Spiacente, ma non ricordo di aver mai avuto un simile fastidio. ";
+	$response = "Vieni con me donna, non parlare. L'unica parola che ti è permessa è "si". Qualsiasi altra cosa tu dica è morte, non per te ma per i tuoi compagni. Non chiedere niente non raccontare niente, tu non hai nessun diritto ... Comprendilo bene, questa non è una negoziazione, è un ordine, donna.";
 }
-if (strpos($text, 'perso') !== false) 
+if (strpos($text, 'fine') !== false) 
 {
-	$response = "Non ammettere mai la sconfitta e chiedere una morte rapida! Muori prima, poi ammetti la sconfitta! Se sei sconfitto ma non sei morto, significa solo che sei stato fortunato! A quel punto, pensa solo alla sopravvivenza! Sopravvivi e pensa solo a uccidere colui che non è riuscito a ucciderti!";
+	$response = "Gli è rimasta ancora un po' di reatsu... ma questo è il suo limite... è la fine. la vostra fortuna è finita.. il sole è già tramontato nel palmo delle nostre mani!";
 }
-elseif (strpos($text, "stanco") !== false)
+elseif (strpos($text, "mort") !== false)
 {
-	$response = "Mi dispiace ma non mi interessano i deboli che non possono più combattere. Inoltre, non ho alcun obbligo di finirti.";
+	$response = "Cosa vuoi sentirti dire da me...? non preuccuparti è sicuramente vivo? scordatelo.io non sono qui per coccolarti.non ti capisco tanto prima o poi tutti i tuoi compagni verranno annientati, che uno muoia prima o dopo non fa differenza, avrebbero dovuto prevederlo fin dall'inizio. Se non ci sono riusciti la colpa risiede nella loro imbecillità, dovresti riderci sopra e basta pensando a quanto sono stupidi perche non ci riesci?.... mi sarei arrabbiato davanti alla stupidità di chi si precipita qui senza nemmeno pensare a soppesare le proprie abilità";
 
 }
+elseif (strpos($text, "vittoria") !== false)
+{
+	$response = "Anche se ti alzi mille volte, non ci sarà alcuna vittoria per nessuno di voi. ";
+
+}
+elseif (strpos($text, "cuore") !== false)
+{
+	$response = "Cuori, dite? Voi umani siete sempre così veloci nel parlare di tali cose. Come se portaste i vostri cuori nei palmi delle mani. Ma questo mio occhio percepisce tutto. Non c'è niente che questo occhio non riesca a vedere, se non lo vede, allora non esiste. Questo è il presupposto in base al quale ho sempre combattuto. Che cos'è questo cuore? Se ti strappo quel torace, lo vedrò lì? Se Apro il tuo cranio, lo vedrò lì?";
+
+}
+elseif (strpos($text, "debole") !== false)
+{
+	$response = "Non lasciarti scuotere. Non indebolire la tua posizione. Apri i tuoi sensi. E non abbassare la guardia per un istante.";
+
+}
+elseif (strpos($text, "somigli") !== false)
+{
+	$response = "Non importa quanto il tuo aspetto e le tue tecniche possano assomigliare a quelli di un Arrancar, sei lontano da noi come la Terra dal cielo. È un percorso comprensibile per gli umani e per gli Shinigami che desiderano ottenere il potere di imitare gli Hollow. Tuttavia, non ti permetterà mai di stare allo stesso livello di Arrancar.";
+
+}
+elseif (strpos($text, "disper") !== false)
+{
+	$response = "Le tue parole sono quelle di un uomo che non conosce la vera disperazione. Se non lo conosci, allora permettimi di insegnarti. Questo è il volto della vera disperazione.";
+
+}
+elseif (strpos($text, "sciocc") !== false)
+{
+	$response = "Sei uno sciocco. Cerchi volentieri di sfidare un avversario molto più potente di te tanto da provare una paura primordiale nel tuo essere. Incomprensibile. Se questo è il lavoro del cuore di cui parli, quindi è perché posseggono questo cuore che gli umani si fanno del male, perché possiedi questo cuore che perdi la vita?.";
+
+}
+elseif (strpos($text, "pietà") !== false)
+{
+	$response = "Capisco. Quindi non mostri pietà. Proprio come Hollow";
+
+}
+
 // elseif (strpos($text, "comandi Zaraki Kenpachi")
 // {
 //	$response = "ciao
